546147ba9a416bfae86d7689a5bec0db17bd859b
ZaenalRifai15 <zaenalrifai55@gmail.com>
2024-12-16 15:00:52 +0700

    login dan register

--- project_product/products/admin.py
+++ project_product/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+# admin.py
+from django.contrib import admin
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'berat', 'kondisi')
+    search_fields = ('name', 'kondisi') 
+    list_filter = ('kondisi',)
+    ordering = ('name',) 
--- project_product/products/models.py
+++ project_product/products/models.py
@@ -6,6 +6,10 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product_images/')
+    deskripsi = models.TextField(null=True, blank=True)
+    berat = models.IntegerField(default=1)
+    kondisi = models.CharField(max_length=50, default='baru panen')
+
 
     def __str__(self):
         return self.name
